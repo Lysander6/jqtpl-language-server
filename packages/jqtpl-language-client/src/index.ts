8aee7cb58063412dfc32942701e071941e8b63b8
Lysander <lysander6@gmail.com>
2022-12-23 22:30:20 +0100

    basic LSP client

--- packages/jqtpl-language-client/src/index.ts
+++ packages/jqtpl-language-client/src/index.ts
@@ -1,1 +1,40 @@
-console.info("Hello, World!");
+import { ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(_context: ExtensionContext) {
+  const serverModule = "jqtpl-language-server";
+
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: { module: serverModule, transport: TransportKind.ipc },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ pattern: "*.jqtpl" }],
+  };
+
+  client = new LanguageClient(
+    "jqtpl-language-server",
+    "JQTPL Langauge Server",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | void {
+  if (!client) {
+    return undefined;
+  }
+
+  return client.stop();
+}
