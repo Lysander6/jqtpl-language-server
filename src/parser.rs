13e3f1cd0da75d07458f64974cbe33fc5fb8c3af
Lysander <lysander6@gmail.com>
2023-01-21 23:22:49 +0100

    Keep spans with parsed tokens/expressions

--- src/parser.rs
+++ src/parser.rs
@@ -21,62 +21,72 @@ enum Token {
 struct Spanned<T>(T, Range<usize>);
 
 #[allow(dead_code)]
-fn parser() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
+fn parser() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
     let code = choice((none_of("\n\r}"), just('}').padded_by(none_of("}"))))
         .repeated()
         .at_least(1);
 
-    let content = take_until(just("{").rewind());
+    let content =
+        take_until(just("{").rewind()).map_with_span(|_, span| Spanned(Token::Content, span));
 
-    let r#if = text::keyword("if").then_ignore(code.clone()).to(Token::If);
+    let r#if = text::keyword("if")
+        .then_ignore(code.clone())
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::If, span));
 
     let r#else = text::keyword("else")
         .then_ignore(code.clone().or_not())
-        .to(Token::Else);
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Else, span));
 
-    let end_if = just("/if").to(Token::EndIf);
+    let end_if = just("/if")
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::EndIf, span));
 
     let comment = just("!")
         .then_ignore(code.clone().or_not())
-        .to(Token::Comment);
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Comment, span));
 
     let var = text::keyword("var")
         .then_ignore(code.clone())
-        .to(Token::Var);
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Var, span));
 
     let each = text::keyword("each")
         .then_ignore(code.clone())
-        .to(Token::Each);
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Each, span));
 
-    let end_each = just("/each").to(Token::EndEach);
+    let end_each = just("/each")
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::EndEach, span));
 
     let html = text::keyword("html")
         .then_ignore(code.clone())
-        .to(Token::Html);
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Html, span));
 
     let tmpl = text::keyword("tmpl")
         .then_ignore(code.clone())
-        .to(Token::Tmpl);
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Tmpl, span));
 
-    let print = just("=").then_ignore(code.clone()).to(Token::Print);
+    let print = just("=")
+        .then_ignore(code.clone())
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|_, span| Spanned(Token::Print, span));
+
+    let unknown = code
+        .clone()
+        .delimited_by(just("{{"), just("}}"))
+        .map_with_span(|s, span| Spanned(Token::Unknown(String::from_iter(s.iter())), span));
 
     let directive = choice((
-        r#if,
-        r#else,
-        end_if,
-        each,
-        end_each,
-        html,
-        comment,
-        var,
-        tmpl,
-        print,
-        code.clone()
-            .map(|s| Token::Unknown(String::from_iter(s.iter()))),
-    ))
-    .delimited_by(just("{{"), just("}}"));
+        r#if, r#else, end_if, each, end_each, html, comment, var, tmpl, print, unknown,
+    ));
 
-    choice((directive, content.to(Token::Content)))
+    choice((directive, content))
         // .recover_with(skip_then_retry_until(['{', '}']).consume_end())
         .repeated()
         .then_ignore(end())
@@ -85,6 +95,7 @@ fn parser() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use Token::*;
 
     #[test]
     fn empty_string() {
@@ -93,15 +104,15 @@ mod tests {
     }
 
     #[test]
-    fn braces() {
+    fn if_() {
         let result = parser().parse("{{if }}");
-        assert_eq!(result, Ok(vec![Token::If]));
+        assert_eq!(result, Ok(vec![Spanned(If, 0..7)]));
     }
 
     #[test]
     fn else_() {
         let result = parser().parse("{{else}}");
-        assert_eq!(result, Ok(vec![Token::Else]));
+        assert_eq!(result, Ok(vec![Spanned(Else, 0..8)]));
     }
 
     #[test]
@@ -115,11 +126,11 @@ mod tests {
         assert_eq!(
             result,
             Ok(vec![
-                Token::If,
-                Token::Content,
-                Token::Else,
-                Token::Content,
-                Token::EndIf
+                Spanned(If, 0..10),
+                Spanned(Content, 10..35),
+                Spanned(Else, 35..43),
+                Spanned(Content, 43..70),
+                Spanned(EndIf, 70..77),
             ])
         );
     }
