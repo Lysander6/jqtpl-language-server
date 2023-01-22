05321ad0117e9526e55d746b517404722938962d
Lysander <lysander6@gmail.com>
2023-01-22 14:26:50 +0100

    More correct nomenclature

--- src/parser.rs
+++ src/parser.rs
@@ -2,65 +2,54 @@ use chumsky::prelude::*;
 use std::ops::Range;
 
 #[derive(Clone, Debug, PartialEq)]
-enum Token {
-    Content,
+enum Stmt {
     Comment,
+    Content,
     Each,
-    EndEach,
-    Html,
-    Var,
-    If,
+    EachEnd,
     Else,
-    EndIf,
-    Tmpl,
+    Html,
+    If,
+    IfEnd,
     Print,
+    Tmpl,
     Unknown(String),
+    Var,
 }
 
 #[derive(Debug, PartialEq)]
 struct Spanned<T>(T, Range<usize>);
 
 #[allow(dead_code)]
-fn parser() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
-    use Token::*;
+fn parser() -> impl Parser<char, Vec<Spanned<Stmt>>, Error = Simple<char>> {
+    use Stmt::*;
 
     let code = choice((none_of("\n\r}"), just('}').padded_by(none_of("}"))))
         .repeated()
         .at_least(1);
-
     let content = take_until(just("{").rewind()).map_with_span(|_, span| Spanned(Content, span));
 
-    let r#if = text::keyword("if").then_ignore(code.clone()).to(If);
-
+    let comment = just("!").then_ignore(code.clone().or_not()).to(Comment);
+    let each = text::keyword("each").then_ignore(code.clone()).to(Each);
+    let each_end = just("/each").to(EachEnd);
     let r#else = text::keyword("else")
         .then_ignore(code.clone().or_not())
         .to(Else);
-
-    let end_if = just("/if").to(EndIf);
-
-    let comment = just("!").then_ignore(code.clone().or_not()).to(Comment);
-
+    let html = text::keyword("html").then_ignore(code.clone()).to(Html);
+    let r#if = text::keyword("if").then_ignore(code.clone()).to(If);
+    let if_end = just("/if").to(IfEnd);
+    let print = just("=").then_ignore(code.clone()).to(Print);
+    let tmpl = text::keyword("tmpl").then_ignore(code.clone()).to(Tmpl);
+    let unknown = code.clone().map(|s| Unknown(String::from_iter(s.iter())));
     let var = text::keyword("var").then_ignore(code.clone()).to(Var);
 
-    let each = text::keyword("each").then_ignore(code.clone()).to(Each);
-
-    let end_each = just("/each").to(EndEach);
-
-    let html = text::keyword("html").then_ignore(code.clone()).to(Html);
-
-    let tmpl = text::keyword("tmpl").then_ignore(code.clone()).to(Tmpl);
-
-    let print = just("=").then_ignore(code.clone()).to(Print);
-
-    let unknown = code.clone().map(|s| Unknown(String::from_iter(s.iter())));
-
-    let directive = choice((
-        r#if, r#else, end_if, each, end_each, html, comment, var, tmpl, print, unknown,
+    let statement = choice((
+        comment, each, each_end, r#else, html, r#if, if_end, print, tmpl, unknown, var,
     ))
     .delimited_by(just("{{"), just("}}"))
     .map_with_span(|t, span| Spanned(t, span));
 
-    choice((directive, content))
+    choice((statement, content))
         // .recover_with(skip_then_retry_until(['{', '}']).consume_end())
         .repeated()
         .then_ignore(end())
@@ -69,7 +58,7 @@ fn parser() -> impl Parser<char, Vec<Spanned<Token>>, Error = Simple<char>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use Token::*;
+    use Stmt::*;
 
     #[test]
     fn empty_string() {
@@ -104,7 +93,7 @@ mod tests {
                 Spanned(Content, 10..35),
                 Spanned(Else, 35..43),
                 Spanned(Content, 43..70),
-                Spanned(EndIf, 70..77),
+                Spanned(IfEnd, 70..77),
             ])
         );
     }
